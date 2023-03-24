d9d0e2b3d4a8296210b4410061878a024ac36c0c
Rachel Isaac <rachelisaac995@gmail.com>
2023-03-24 04:41:00 +0400

    Add sidebar component and stye it

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Sidebar.scss';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faFileDownload,
+  faSuitcase,
+} from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 
@@ -11,6 +20,66 @@ function Sidebar() {
         <img src={LogoS} alt="logo" />
         <img className="sub-logo" src={LogoSubtitle} alt="logo name" />
       </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <ul>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="linkedin-link"
+              href="https://www.linkedin.com/in/rachelisaac13/"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="github-link"
+              href="https://www.linkedin.com/in/rachelisaac13/"
+            >
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="resume-link"
+              href="https://docs.google.com/document/d/1O0iYmCCtfvmwrUSw1KS9il9L4cI1vzwjFkWGlBlwYRI/edit?usp=share_link"
+            >
+              <FontAwesomeIcon icon={faFileDownload} color="#4d4d4e" />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
