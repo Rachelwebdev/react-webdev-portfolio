ff2904d88c95db36a2a331d0c38ebbd702729c6e
Rachel Isaac <rachelisaac995@gmail.com>
2023-04-11 06:46:27 +0400

    Add project data to data file

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,6 +2,15 @@ import React from 'react';
 import Loader from 'react-loaders';
 import './Project.scss';
 import Sidebar from '../Sidebar/Sidebar';
+import bookstore from '../../assets/images/project/bookstore.png';
+import mathMagician from '../../assets/images/project/calculator.png';
+import cinemy from '../../assets/images/project/cinemy.png';
+import crypto from '../../assets/images/project/crypto.png';
+import dropdown from '../../assets/images/project/dropdown.png';
+import leaderboard from '../../assets/images/project/leaderboard.png';
+import surpriseKitchen from '../../assets/images/project/suprisekitchen.png';
+import ultrawave from '../../assets/images/project/ultrawave.png';
+import worldChef from '../../assets/images/project/worldchef.jpg';
 
 function Project() {
   return (
